9b3b1ee7a24fc55f9c60b367cc48120db432cf03
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2020-11-10 15:06:46 +0100

    index setup

---
+++ index.js
@@ -0,0 +1,18 @@
+//import 
+import express from 'express';
+import cors from 'cors';
+
+//app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+//CORS
+app.use(cors());
+
+//body parser
+app.use(express.json());  
+
+
+//listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
+
