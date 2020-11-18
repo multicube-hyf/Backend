20ad322cf0451260fca370850f3947b6d09c9b46
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2020-11-18 17:23:26 +0100

    middleware added to user routes

--- routes/user-test.js
+++ routes/user-test.js
@@ -4,11 +4,22 @@
 */
 
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 
 const {createUser, getUsers, getStudents, getStudent} = require('../controllers/user-test-controller');
+const {validateInputs} = require('../middlewares/validate-inputs');
 
-router.post('/register', createUser);
+router.post('/register',
+[  //middleware
+    check('username', 'username is mandatory').not().isEmpty(),
+    check('name', 'Name is mandatory').not().isEmpty(),
+    check('lastName', 'lastname is mandatory').not().isEmpty(),
+    check('role', 'role is mandatory').not().isEmpty(),
+    check('password', 'Password should contain at least 6 characters').isLength({min:6}),
+    validateInputs
+  ], 
+    createUser);
 router.get('/', getUsers);
 router.get('/students', getStudents);
 router.get('/students/:id', getStudent);
