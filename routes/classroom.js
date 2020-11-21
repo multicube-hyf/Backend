1d3bcba668d11fd7a7d29aba86e43b70b3b8af6c
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2020-11-21 15:58:23 +0100

    middewares added to classroom routes

--- routes/classroom.js
+++ routes/classroom.js
@@ -4,15 +4,33 @@
 */
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
-const {createClassroom, getClassrooms, getClassroom, deleteClass, updateClassroom} = require('../controllers/classroomController');
+const {
+	createClassroom,
+	getClassrooms,
+	getClassroom,
+	deleteClass,
+	updateClassroom,
+} = require('../controllers/classroomController');
+
+const { validateInputs } = require('../middlewares/validate-inputs');
+
 const { validateJWT } = require('../middlewares/validate-jwt');
 
-
-router.get('/', validateJWT,getClassrooms);
+router.get('/', validateJWT, getClassrooms);
 router.get('/:id', validateJWT, getClassroom);
-router.post('/', validateJWT, createClassroom);
-router.put('/:id', validateJWT,updateClassroom);
-router.delete('/:id', validateJWT,deleteClass);
+router.post(
+	'/',
+	[
+		//middlewares
+		check('title', 'title is mandatory').not().isEmpty(),
+		validateInputs,
+		validateJWT,
+	],
+	createClassroom
+);
+router.put('/:id', validateJWT, updateClassroom);
+router.delete('/:id', validateJWT, deleteClass);
 
 module.exports = router;
